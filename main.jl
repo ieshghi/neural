536e2959bbd9a2d5db3db1b86959e65e72f5a1ac
Iraj Eshghi <ieshghi@github.com>
2019-05-21 22:03:59 -0400

    runs, doesnt seem to be learning. Add testing?

--- main.jl
+++ main.jl
@@ -14,10 +14,10 @@ function init()
 	nin = 28*28; #Size of the input, in this case a 28*28 image
 	nhid = [15]; #number of hidden layer nodes for each layer
 	nout = 10; #number of output layer nodes
-	w = Matrix{Array{Float64}}(undef,length(nhid),1);
-	b = Matrix{Array{Float64}}(undef,length(nhid),1);
+	w = Matrix{Array{Float64}}(undef,1+length(nhid),1);
+	b = Matrix{Array{Float64}}(undef,1+length(nhid),1);
 	s = [nin;nhid;nout];
-	for i = 1:length(nhid)
+	for i = 1:(1+length(nhid))
 		j = i+1;
 		w[i] = rand(s[j],s[i]);
 		b[i] = rand(s[j]);
@@ -42,17 +42,28 @@ function loadinputs()
 	train_dat = train_dat[3];
 	labs = IDX.load("data/train-labels-idx1-ubyte");
 	labs = labs[3];
-	return train_dat,labs
+
+	l = Matrix{Array{Float64}}(undef,length(labs),1);
+	d = Matrix{Array{Float64}}(undef,length(labs),1);
+	for i = 1:length(labs)
+		d[i] = zeros(28*28);
+		q = train_dat[:,:,i];
+		d[i] = q[:];
+		l[i] = zeros(10);
+		l[i][Int(labs[i])+1]=1;
+	end
+
+	return d,l
 end
 
-function training(eps)
+function training(epochs)
 	dat,labs = loadinputs();
 	w,b = init();
 	frac = 0.1;
-	eta = 0.01;
-	epoch = 1;
-	while cost(dat,labs,w,b,frac)>eps
-		println("Epoch: ",epoch)
+	eta = 3;
+	for epoch = 1:epochs
+		err = cost(dat,labs,w,b,frac);
+		println("Epoch: ",epoch," error = ",err)
 		w,b = update(dat,labs,w,b,eta,frac);
 	end
 	return w,b
@@ -62,9 +73,13 @@ function update(dat,labs,w,b,eta,frac)
 	subdat,sublabs = stoch_samp(dat,labs,frac);
 	gw = Matrix{Array{Float64}}(undef,length(w),1);
 	gb = Matrix{Array{Float64}}(undef,length(w),1);
-
-	for i = 1:size(subdat,3)
-		dw,db = backprop(subdat[:,:,i],sublabs[i]);
+	dw,db = backprop(subdat[1],sublabs[1],w,b);
+	for j = 1:length(w)
+		gw[j] = dw[j];
+		gb[j] = db[j];
+	end
+	for i = 2:size(subdat,3)
+		dw,db = backprop(subdat[i],sublabs[i],w,b);
 		for j = 1:length(w)
 			gw[j] = gw[j] + dw[j];
 			gb[j] = gb[j] + db[j];
@@ -82,22 +97,22 @@ function cost(dat,labs,w,b,frac)
 	c = 0;
 	subdat,sublabs = stoch_samp(dat,labs,frac);
 	for i = 1:size(subdat,3)
-		guess = network(subdat[:,:,i],w,b);
-		c = c+(guess-sublabs[i])^2;
+		guess = network(subdat[i],w,b);
+		c = c+sum((guess-sublabs[i]).^2);
 	end
 
 	return c./length(sublabs)
 end
 
 function stoch_samp(a,b,frac)
-	n = length(a[1,1,:]);
+	n = length(a);
 	if frac == 0
 		ind = floor(rand()*n);
-		aout = a[:,:,ind];
+		aout = a[ind];
 		bout = b[ind];
 	else
 		ind = rand(n).<frac;
-		aout = a[:,:,ind];
+		aout = a[ind];
 		bout = b[ind];
 	end
 	return aout,bout
@@ -112,25 +127,25 @@ function backprop(x,y,w,b)
 	end
 
 	act = x;
-	acthist = [x];
 	zs = Matrix{Array{Float64}}(undef,length(w),1);
+	acthist = Matrix{Array{Float64}}(undef,1+length(w),1);
+	acthist[1] = x;
 	for i = 1:length(w)
-		z = w[i]*act+b;
+		z = w[i]*act+b[i];
 		zs[i] = z;
 		act = sigmoid.(z);
-		acthist = [acthist,act];
+		acthist[i+1] = act;
 	end
-	
-	delta = (acts[end]-y)*dsigmoid.(zs[end]);
-	nb[end] = delta;
-	nw[end] = delta*(acts[end-1]');
 
-	for j = 1:length(w)
+	delta = (acthist[end]-y).*dsigmoid.(zs[end]);
+	nb[end] = delta;
+	nw[end] = delta.*(acthist[end-1]');
+	for j = 1:(length(w)-1)
 		z = zs[end-j];
 		spz = dsigmoid.(z);
-		delta = ((w[end-j+1]')*delta)*spz;
+		delta = ((w[end-j+1]')*delta).*spz;
 		nb[end-j] = delta;
-		nw[end-j] = delta*acts[end-j-1];
+		nw[end-j] = delta.*acthist[end-j-1]';
 	end
 	return nw,nb
 end
